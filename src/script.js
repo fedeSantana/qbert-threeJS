27b512c091cfa0f9db9a20649a1fcd743e68fec5
Santiago Santana <santana.santiago@gmail.com>
2021-12-03 18:49:14 -0300

    changes

--- src/script.js
+++ src/script.js
@@ -1,17 +1,27 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-import { ColorGUIHelper } from '../utils/colorGUIHelper'
-import { piramide } from '../utils/piramide'
-import { box } from '../utils/piramide'
-import { rotateObject } from '../utils/rotate'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import { ColorGUIHelper } from "../utils/colorGUIHelper";
+import { piramide } from "../utils/piramide";
+import { box } from "../utils/piramide";
+import { rotateObject } from "../utils/rotate";
+import { CameraHelper } from "three";
+
+class Qbert {
+  constructor(posicion, lifes) {
+    this.posicion = posicion;
+    this.lifes = lifes;
+  }
+
+  move(position) {}
+}
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 /**
  * Debug
@@ -20,114 +30,232 @@ const gui = new dat.GUI();
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const map = piramide(7);
+const color1 = new THREE.Color(`#2f4845`);
+const color2 = new THREE.Color(`#56a999`);
+const color3 = new THREE.Color(`#5742e5`);
+
+const colors = {
+  x: color1,
+  y: color2,
+  z: color3,
+};
+
+let map = piramide(7, colors);
 scene.add(map);
 
-const color1 = new THREE.Color(`hsl( 20, 40%, 50%)`);
-const color2 = new THREE.Color(`hsl( 80, 70%, 60%)`);
-const color3 = new THREE.Color(`hsl( 140 , 70%, 60%)`);
-
-const geometrys = {
-    x: new THREE.PlaneGeometry(1, 1),
-    y: new THREE.PlaneGeometry(1, 1),
-    z: new THREE.PlaneGeometry(1, 1),
-}
-
-const materials = {
-    x: new THREE.MeshStandardMaterial({
-        color: color1,
-        flatShading: true,
-        side: THREE.DoubleSide
-    }),
-    y: new THREE.MeshStandardMaterial({
-        color: color2,
-        flatShading: true,
-        side: THREE.DoubleSide
-    }),
-    z: new THREE.MeshStandardMaterial({
-        color: color3,
-        flatShading: true,
-        side: THREE.DoubleSide
-    }),
-}
-
-
 // Camera
-const camera = new THREE.OrthographicCamera(-8, 8, 8, - 8, 0.1, 1000)
+const camera = new THREE.OrthographicCamera(-7, 12, 12, -7, 0.1, 1000);
 
-camera.position.set(-100, 80, -50);
+camera.position.set(-86.5, 86.1, -79);
 camera.lookAt(3.5, 3.5, 3.5);
 
-scene.add(camera)
-
+scene.add(camera);
 
 scene.background = new THREE.Color("#000");
 
+// Lights
+
 // Create a directional light
 const ambientLight = new THREE.HemisphereLight(0xddeeff, 0x202020, 9);
+
 let mainLight = new THREE.DirectionalLight(0xffffff, 3.0);
 scene.add(ambientLight);
 
 // move the light back and up a bit
 mainLight.position.set(5, 5, 5);
 
-const color = 0xff0000;
-const intensity = 1;
-const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(5, 5, 5);
-light.target.position.set(-5, 0, 0);
+const lightColor = new THREE.Color(`#2f4845`);
+
+const lightOptions = {
+  color: lightColor,
+  intensity: 1,
+  position: {
+    x: 5,
+    y: 5,
+    z: 5,
+  },
+  target: {
+    x: -5,
+    y: 5,
+    z: 5,
+  },
+};
+
+let light = new THREE.DirectionalLight(
+  lightOptions.color,
+  lightOptions.intensity
+);
+light.position.set(
+  lightOptions.position.x,
+  lightOptions.position.y,
+  lightOptions.position.z
+);
+light.target.position.set(
+  lightOptions.target.x,
+  lightOptions.target.y,
+  lightOptions.target.z
+);
 scene.add(light);
 scene.add(light.target);
 
-const axesHelper = new THREE.AxesHelper(5);
+let DL_helper = new THREE.DirectionalLightHelper(light, 5);
+scene.add(DL_helper);
+
+// AxeHelper
+const axesHelper = new THREE.AxesHelper(7);
 scene.add(axesHelper);
-
-
 
 // remember to add the light to the scene
 //scene.add(ambientLight, mainLight);
 
-gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
-gui.add(light, 'intensity', 0, 2, 0.01);
-gui.add(light.target.position, 'x', -10, 10);
-gui.add(light.target.position, 'z', -10, 10);
-gui.add(light.target.position, 'y', 0, 10);
-
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas, antialias: true
-})
-renderer.setSize(sizes.width, sizes.height)
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.gammaOutput = true;
 renderer.physicallyCorrectLights = true;
 
-const controls = new OrbitControls(camera, canvas)
+const controls = new OrbitControls(camera, canvas);
 
 controls.target.set(3.5, 3.5, 3.5);
 
+controls.update();
 
 // Animate
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
+
+const look = { x: 3.5, y: 3.5, z: 3.5 };
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update objects
-    //mesh.rotation.y = elapsedTime;
+  // Update objects
+  //mesh.rotation.y = elapsedTime;
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
+
+const gui_folder_light = gui.addFolder(`Light`);
+const gui_folder_camera = gui.addFolder(`Camera`);
+const gui_folder_camera_size = gui.addFolder("Camera size");
+const gui_folder_cube = gui.addFolder(`Cube`);
+const gui_folder_helpers = gui.addFolder(`Helpers`);
+
+let cameraHelper = new THREE.CameraHelper(camera);
+scene.add(cameraHelper);
+
+const update_camera = () => {
+  camera.lookAt(look.x, look.y, look.z);
+  controls.update();
+  cameraHelper.update();
+  camera.updateProjectionMatrix();
+};
+
+const update_color = () => {
+  scene.remove(map);
+  map = piramide(7, colors);
+  scene.add(map);
+};
+
+const update_light = () => {
+  light.color = lightOptions.color;
+  light.intensity = lightOptions.intensity;
+  light.position.x = lightOptions.position.x;
+
+  light.position.y = lightOptions.position.y;
+  light.position.z = lightOptions.position.z;
+
+  light.target.position.x = lightOptions.target.x;
+  light.target.position.y = lightOptions.target.y;
+  light.target.position.z = lightOptions.target.z;
+
+  DL_helper.update();
+};
+
+gui_folder_camera_size.add(camera, "top", -10, 10).onChange(update_camera);
+gui_folder_camera_size.add(camera, "right", -10, 10).onChange(update_camera);
+gui_folder_camera_size.add(camera, "bottom", -10, 10).onChange(update_camera);
+gui_folder_camera_size.add(camera, "left", -10, 10).onChange(update_camera);
+gui_folder_camera_size.add(camera, "near", -10, 10).onChange(update_camera);
+gui_folder_camera_size.add(camera, "far", -200, 200).onChange(update_camera);
+
+gui_folder_camera
+  .add(camera.position, "x", -200, 0, 0.1)
+  .name("position X")
+  .onChange(update_camera);
+gui_folder_camera
+  .add(camera.position, "y", -100, 100, 0.1)
+  .name("position Y")
+  .onChange(update_camera);
+gui_folder_camera
+  .add(camera.position, "z", -100, 100, 0.1)
+  .name("position Z")
+  .onChange(update_camera);
+
+gui_folder_light
+  .add(lightOptions, "intensity", 0, 5, 0.01)
+  .onChange(update_light);
+
+gui_folder_light
+  .add(lightOptions.position, "x", -40, 40)
+  .name("position X")
+  .onChange(update_light);
+gui_folder_light
+  .add(lightOptions.position, "y", -40, 40)
+  .name("position Y")
+  .onChange(update_light);
+gui_folder_light
+  .add(lightOptions.position, "z", -40, 40)
+  .name("position Z")
+  .onChange(update_light);
+gui_folder_light
+  .add(lightOptions.target, "x", -40, 40)
+  .name("target X")
+  .onChange(update_light);
+gui_folder_light
+  .add(lightOptions.target, "y", -40, 40)
+  .name("target Y")
+  .onChange(update_light);
+gui_folder_light
+  .add(lightOptions.target, "z", -40, 40)
+  .name("target Z")
+  .onChange(update_light);
+gui_folder_light
+  .addColor(new ColorGUIHelper(lightOptions, "color"), "value", "color")
+  .name("color")
+  .onChange(update_light);
+
+gui_folder_cube
+  .addColor(new ColorGUIHelper(colors, "x"), "value", "right face")
+  .name("color")
+  .onChange(update_color);
+
+gui_folder_cube
+  .addColor(new ColorGUIHelper(colors, "y"), "value", "top face")
+  .name("color")
+  .onChange(update_color);
+
+gui_folder_cube
+  .addColor(new ColorGUIHelper(colors, "z"), "value", "left face")
+  .name("color")
+  .onChange(update_color);
+
+gui_folder_helpers.add(cameraHelper, "visible", true, false).name("camera");
+gui_folder_helpers.add(DL_helper, "visible", true, false).name("light");
+gui_folder_helpers.add(axesHelper, "visible", true, false).name("axes");
--- utils/piramide.js
+++ utils/piramide.js
@@ -1,99 +1,91 @@
-import * as THREE from 'three'
-import { rotateObject } from './rotate'
-
+import * as THREE from "three";
+import { rotateObject } from "./rotate";
 
 const material = new THREE.MeshStandardMaterial({
-    color: 0xff3333,
-    flatShading: true
+  color: 0xff3333,
+  flatShading: true,
 });
 
 export function box(position, geometrys, materials) {
-    const group = new THREE.Group();
+  const group = new THREE.Group();
 
-    const plane = new THREE.Mesh(geometrys.x, materials.x);
-    const plane2 = new THREE.Mesh(geometrys.y, materials.y);
-    const plane3 = new THREE.Mesh(geometrys.z, materials.z);
+  const plane = new THREE.Mesh(geometrys.x, materials.x);
+  const plane2 = new THREE.Mesh(geometrys.y, materials.y);
+  const plane3 = new THREE.Mesh(geometrys.z, materials.z);
 
-    plane.position.y = 0.5;
-    plane.position.z = 0.5;
+  plane.position.y = 0.5;
+  plane.position.z = 0.5;
 
-    plane2.position.y = 1;
-    plane2.position.z = 0.5;
-    plane2.position.x = 0.5;
+  plane2.position.y = 1;
+  plane2.position.z = 0.5;
+  plane2.position.x = 0.5;
 
-    plane3.position.y = 0.5;
-    plane3.position.z = 0;
-    plane3.position.x = 0.5;
+  plane3.position.y = 0.5;
+  plane3.position.z = 0;
+  plane3.position.x = 0.5;
 
-    rotateObject(plane, 0, 90, 0);
-    rotateObject(plane2, 90, 0, 0);
-    rotateObject(plane3, 0, 0, 90);
+  rotateObject(plane, 0, 90, 0);
+  rotateObject(plane2, 90, 0, 0);
+  rotateObject(plane3, 0, 0, 90);
 
-    group.add(plane);
-    group.add(plane2);
-    group.add(plane3);
+  group.add(plane);
+  group.add(plane2);
+  group.add(plane3);
 
-    console.log(position);
-    group.translateX(position.x);
-    group.translateY(position.y);
-    group.translateZ(position.z);
+  console.log(position);
+  group.translateX(position.x);
+  group.translateY(position.y);
+  group.translateZ(position.z);
 
-    return group;
+  return group;
 }
 
-
-function row(position, start, end) {
-
-    
-const geometrys = {
+function row(position, start, end, colors) {
+  const geometrys = {
     x: new THREE.PlaneGeometry(1, 1),
     y: new THREE.PlaneGeometry(1, 1),
     z: new THREE.PlaneGeometry(1, 1),
+  };
+
+  const group = new THREE.Group();
+
+  // function box(position, size, res, material, tinte)
+  // box(position, geometrys, materials)
+  for (let i = start; i < end; i++) {
+    const materials = {
+      x: new THREE.MeshStandardMaterial({
+        color: new THREE.Color(colors.x),
+        flatShading: true,
+        side: THREE.DoubleSide,
+      }),
+      y: new THREE.MeshStandardMaterial({
+        color: new THREE.Color(colors.y),
+        flatShading: true,
+        side: THREE.DoubleSide,
+      }),
+      z: new THREE.MeshStandardMaterial({
+        color: new THREE.Color(colors.z),
+        flatShading: true,
+        side: THREE.DoubleSide,
+      }),
+    };
+
+    const pos = new THREE.Vector3(position.x, position.y + i, position.z + i);
+    const cube = box(pos, geometrys, materials);
+
+    group.add(cube);
+  }
+
+  return group;
 }
 
-    const group = new THREE.Group();
-    
-    // function box(position, size, res, material, tinte)   
-    // box(position, geometrys, materials)
-    for (let i = start; i < end; i++) {
-        const color1 = new THREE.Color(`#2f4845`);
-        const color2 = new THREE.Color(`#56a999`);
-        const color3 = new THREE.Color(`#5742e5`);
+export function piramide(cant, colors) {
+  const group = new THREE.Group();
 
-        const materials = {
-            x: new THREE.MeshStandardMaterial({
-                color: color1,
-                flatShading: true,
-                side: THREE.DoubleSide
-            }),
-            y: new THREE.MeshStandardMaterial({
-                color: color2,
-                flatShading: true,
-                side: THREE.DoubleSide
-            }),
-            z: new THREE.MeshStandardMaterial({
-                color: color3,
-                flatShading: true,
-                side: THREE.DoubleSide
-            }),
-        }
+  for (let i = 0; i < cant; i++) {
+    const columna = row({ x: -i, y: 0, z: i }, 0, cant - i, colors);
+    group.add(columna);
+  }
 
-        const pos = new THREE.Vector3(position.x, position.y + i, position.z + i);
-        const cube = box(pos, geometrys, materials);
-
-        group.add(cube);
-    }
-
-    return group;
-}
-
-export function piramide(cant) {
-    const group = new THREE.Group();
-
-    for (let i = 0; i < cant; i++) {
-        const columna = row({ x: -i, y: 0, z: i }, 0, cant - i);
-        group.add(columna);
-    }
-
-    return group;
+  return group;
 }
